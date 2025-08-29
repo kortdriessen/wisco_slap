3fc7f4210d01124d9db9c5aef86605c1e5d01858
Kort Driessene <driessenkort@gmail.com>
2025-08-29 18:27:29 -0500

    init: wisco_slap

--- src/wisco_slap/__init__.py
+++ src/wisco_slap/__init__.py
@@ -1,2 +1,7 @@
-def hello() -> str:
-    return "Hello from wisco-slap!"
+# Blessed top-level API
+from .core.SlapData import SlapData
+
+# Make subpackages appear under `wisco_slap.`
+from . import img as img
+
+__all__ = ['SlapData', 'img']
