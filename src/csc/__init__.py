096942c110f4c49158eecc198b64b011fc0dcf0c
Kort Driessene <driessenkort@gmail.com>
2025-09-11 10:55:55 -0500

    reorg after removing csc to separate package

--- src/csc/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import (
-    defs,
-    utils,
-)
-
-__all__ = ['utils', 'defs'] #TODO (?)
--- src/csc/defs.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-from pathlib import Path
-import os
-
-# -------- Defaults (edit these) --------
-data_root: str = '/Volumes/slap_mi/slap_mi/data'
